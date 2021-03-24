633bb17f0c2237e249e7fff9b18456767c5c7033
Parita Shah <paritashah@Paritas-MacBook-Pro.local>
2021-03-24 16:20:08 -0500

    gallery photos not working

---
+++ src/constants/photos.js
@@ -0,0 +1,32 @@
+export const photos = [
+  {
+    src: "CindyWang.jpg",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "../assets/images/CindyWang.jpg",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "../assets/images/CindyWang.jpg",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "../assets/images/CindyWang.jpg",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://tinyurl.com/y6nholho",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://tinyurl.com/y6nholho",
+    width: 4,
+    height: 3
+  }
+];
