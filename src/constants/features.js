f1a565fe239e1568eeef86d0a50f6a197563940c
Melissa Kwan <mwjkwan@gmail.com>
2021-03-27 11:36:08 -0400

    Add initial features data

---
+++ src/constants/features.js
@@ -0,0 +1,111 @@
+const features = {
+  robin_robinson: {
+      "id": "robin_robinson",
+      "name": "Robin Robinson",
+      "year": 2020,
+      "concentration": "math",
+      "pronouns": "she/her/hers",
+      "quote": "adsf",
+      "photostat": "https://lh3.googleusercontent.com/pw/ACtC-3dRXFNNk54rpeqlt8eRqT0n5s9JQksN5n8rmrYo8w62uegjtIh-AD5ebrqytcrsC7DJi8j3tMknxb2qC3bgTVYNcdsaxjB7peozsAagYMSKfg2wQqVQHOwV7EoThPfuY3ATQA63R8LagASMQzT7RJPP=w1000-h1400-no?authuser=0"
+  },
+  grace_tian: {
+      "id": "grace_tian",
+      "name": "Grace Tian",
+      "year": 2022,
+      "concentration": "cs+stat",
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/pw/ACtC-3fVNMaYrhpg0v3zGy1_RfMViwfIDV1llY7dMJJEc1wsQaaG_SddFkxn37c3FsNQOt0HXPMmDpAUAHVnVWgv-_ldzp9Sq7yPNRwPOivd1_HBLUzj0oU1P1T62dg7TnxyJLMmlTMTEwGLeCryqK9xCkGF=w1000-h1400-no?authuser=0"
+  },
+  sarika_chawla: {
+      "id": "sarika_chawla",
+      "name": "Sarika Chawla",
+      "year": 2023,
+      "concentration": "cs",
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/pw/ACtC-3e0G7mb0G56x6u_xbGS22Mis2tHo7aQd0YDYljDcqXZTajphfvHP6CfqPM18fxwjZ_6qINkuuPlmHPNxuV6FmgF-RuqgUCAvamq_0SMOSwCNPo6EpKRAaCi0wIWCOUcBsPkuMKlvRW64r_wplTm3qoh=w1000-h1400-no?authuser=0"
+  },
+  amy_jin: {
+      "id": "amy_jin",
+      "name": "Amy Jin",
+      "year": 2022,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/pw/ACtC-3fnR1G4aKYuGj81-DphVNLWaXfrMLLOw_rQaoDf5wstu-NI8LyqhW1WadVO3QmhlylQAmfXZ3D02DqoP31kNg9f3cB_7LnzI9vrZHvVio74cIH-AHnfPB3L4oWKGh40378WTU1FAOCllMI2szb9vm6A=w1000-h1400-no?authuser=0"
+  },
+  emily_chan: {
+      "id": "emily_chan",
+      "name": "Emily Chan",
+      "year": 2021,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/bpfqvMohBZQF4hfZYWiU6wLiiisJRmGW-FJ_uwhp_HjfV9O6htC1IbO_iHPt5Aaz7xEoOjnfB2EZUMw5Q-0enqqE1pSTu0uURa2_2dNPz7yEMv3OOi8hyS6i3wglY-3a9hVny3EiXWOnYnalVA3n1zEmJ6YI8ZRnrzPIIvxFZbGd6_VqzT_F9XBtP8tXOdHA9twjEV5JyKEp19Jaq9QhmaQ6MjrTPMS4xFzUFmX2nYmoa6MZ2Alg0gb6yloit2p6hdQkEnyvJN8OM4MClIcyffwJIwviB-REH8Be4N7sU91hQwFg8KaY9aNFrJwCN41b7_XTyMfwDty6zJpLmj8ddd74TBBKCxus1KEqt3qLqH7cnU0FumGxK-NcZexs1i5_mqfVUKRyLPz4a-C9gTBQRJwE77s2b-znBLQe-2E_d0S1XEylDa1pqhwbH9kD6c1X6gilGJ6_phGJW0O2G7TL4i6Wy3yL00YKUPO1caERjJd7rUlWYzm_a2ZldMZvLe7V0c46qyLdZ-rFIegvMsytAEasNWTNw8fwzDyupukqG9pjSA56N1KjkblQwT1EhrZmr9wQoOJeiUmP8BbUv0U-LoFDymq_fHfrrqehhS3dncgUGanJ9SVH9q3aXkclJHIefgImIHcyGfLO6SzVnKaa0ZarPRzYjn3dia-aNrkzT5Zswhl8ayF0hnfOgawze_Eg_lFwucGSxELF8RFJbLIqQxz-=w1000-h1400-no?authuser=0"
+  },
+  miela_foster: {
+      "id": "miela_foster",
+      "name": "Miela Foster",
+      "year": 2021,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/pw/ACtC-3ciXNowaNn2UdeBGlvhA5PHOoA371uMbaGvKCMKB9txn6IVkC4CY0rzU5F6JL7T0ubnIUYElmKJJHSY-o4NxRELEMhIVHkZOM4SIJr5cjra0msg3Ftljsci3nJLICVLnaBTYwKWDs2XD0kJTGDhU38K=w1000-h1400-no?authuser=0"
+  },
+  cindy_wang: {
+      "id": "cindy_wang",
+      "name": "Cindy Wang",
+      "year": 2024,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/lUmMhIxF_NNF8jfLLSEl6XKutipRnCQttpQ_WdzMVOhG00pUe_fMbm4toiehg3ZGTSQ6evXPIyAUAfk4UsgMT72qWSYRCef7QjX5ecyxGMRT7d_JC_sVXzQlumBzoQTPX-sx-LNnNUWXuXKHnGXNVHterZsGIE72vYIMaHMTiYEhbaYcNWa0l4YLpf4oCdvvWLKr7t3J4Gw8rpKvovCdapXTCvxKPnSut9C3uCnvChZEjISW4_qV1_aD74uY1dlbMoTv6EsrpD32Tqs0bDZ2dwb855-p05su07y0SkWjpBZ3SqlTtxU_H6BW_iJNAeGBw9aI4OqUBy1vDQIDZwWo_PqyB-5_Awt29b9wcdFHvYMWJyNcMv0mhOtW6l3BWedc3L28jbQugSOwyUKIewvpFl5nV-UG0CPFa-X-VphFaCK96_QEJMtnXazK81W0rLUmqx1bnNUVuknAFJxP9hsc7USBm_FYWZ2VZQb45VNJHkr_2PJiTyH8kaUyIh34__vTq3NL0y2S77KbCkbSwLgFjM4GSQpGBGWHqgvd4CgSYr_5L_MWhVCGC4S1IqPqMwGxhz7En4NjJVaH4S3w_ZpiwOUMsC6TOwahFVly_mNhJD5qDOkd5YcJQY-A3M7cXZwyAcrOReA-DRbnOPfx9p9nMGY--BRK3Pi4593EUQ-i72HvLfzBOU9iKZn7wPSunl5-3bIkFPCHGf55qZzhK4hSemRB=w1000-h1400-no?authuser=0"
+  },
+  eric_jjemba: {
+      "id": "eric_jjemba",
+      "name": "Eric Jjemba",
+      "year": 2021,
+      "pronouns": "he/him/his",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/-_i5n03fVv0tUnoWJwb6xH76P6DfPV8AgwYyVNhbxrH_I6PawZ4lm7dm3XARoTSz6Q_TNruDbL5m6JMnkj7OoXnauVfKYBh5tqFZP-qMPES4QIw6RvOs92UPmlIOp1HNi9QWy5doBy3ZjCveYM6po7FtUsNXoBmrkAEk3X9qarSbjbDAtkCDfskvJ1DcQhqOSffRMpQNnxn2ig7I4crWHAPpnrg4atMrYIpmj1oy4EkGwET8-B6BSAO7N1j_P3DRu868qtALyUdhM_1SYHBgVc6xR4HzAg9I-1g_71bGfstvjG7NGkOtGtg2P5mDkfk-N346JuAcY8rWT73oSwScBpQrFU43Dh9DplMzQgfAjOU4xEYGBr0dz1iN4DljChJ8ODZzNFjwUXBRxjxjr0jTMw_kBCaFVMFzvuAGg2UiUMxxpg5Qc7nEKNcjuEoqQYLoiuxXWogYYs987KrkFl68GaE3veFG90Lct84oG7xKkyI-8N5_dHe7Tn2YNFYQY1mqO-_WtiRNsCBv5sgaS_Z5qAGuDi-0QHcHXgocqgQA4r8SmCywezzXNCpaaWV5GQDjKaq6T5aku4PF_QEIbsKVMEONfKHG5mgTd1Z5ZSbhETl9WEKi3bkx-K9P-XEKS8WpWVElwIfKW7jo_n7LpS1YbMu9tMaEwj-Fm1U6hmBLSJBZtHomjdoj9HwukqccWifMfzGAV7RIYv-CPeptHrGb8Jj7=w1000-h1400-no?authuser=0"
+  },
+  amy_jin: {
+      "id": "amy_jin",
+      "name": "Amy Jin",
+      "year": 2022,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/pVttIWXxlZRQWXEMsUPTtTr8i7_JLhpaOQMgfaBXJIFsz0Icoow1CjVzMQNHW10seqnHw5pTZF6Tz4aMT4BLChowLeodhkdyPflszmB5WmAfNXBVxPJK1NP4Q78CW7oj_COwDAQdkh2cm_z0ddJxTSZAgNaEV-ZJUbdk-XnAKLuPHYZFtSLUquBcJ-RPNegFdNI6GLg-JPrSakCajDqU8JUbzCq7YRQDinLbNAtfPh_GXYsh_fmAJKim2teYW79hFqYxGCFifFxeNO9z6Dd3_XOBf3nI1gcq7awBiBdSzRNlfOjm9Gegkm8CD34JCDLEjxhz8MrW-QK0zdivZN2H8oDKhD3MqB4xEFRctGuEDXVlpkEbjFRHQUbC7p3BkQUmL0lJ0PcdAwiIBTe7u4ZZYH8d8IaM6DxvwcfPrki1mlb1mrZb5AUFqE5Ndm4pGZIro7Crjrtg1OnUs_pPLQn1QkufWxUa3OalKEHLmvC-7SCDEfouUzqWF_aqWnEuSNlZWBlfb3e9uTaTzRCnZYpf-Pnl7KJAOgWzAkY986iYG06ycyPaiudYP0AvAaFWUkhldFpwzqxafTFkxwLR8ydngdvRHbf-Xv-nkvHmuvsFvUNTGIWVBlLYtdcLXk3Pvu4O0DfAI76q-pcB0OU5IJP_sq3qIFUkIet5eNJQUxdil-HIHYo6s4knU955l5Tr2Yv7VWUv2_1xrf5i2zqpSVoHzdm1=w1000-h1400-no?authuser=0"
+  },
+  harinni_kannan: {
+      "id": "harinni_kannan",
+      "name": "Harinni Kannan",
+      "year": 2024,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/6L46-PM-pYY8C9OpfLUPFOfU-N9HeOvGf9jtboMB20CnYIOkom_EoRnqqr7eyt3GOBDVG_PASwQlp3mh7SeWace_rAJd4QR-AUCLszbzsvT8OGRTyag0FZ-0jTeRPc2Z8XCA1aJI3lum5tgABF9nEzUf_BpIYYy8FOzeoZRFU8LqimuI-axBU0Wp82GaiKzBSaohXtNA1908B3qz-wxziQywP1DgjUsrNNyWSFPdnZF94l1BoPwcoAVUQg_wrGdE_BJUktfCoM3K0KaKIlqAWiRS9nQ3r0etwOQ3k4OoEJvC_z9r2uMQTUMJOrInGixzzaZbvp6f1z3Kz_alb49zWBcTCx1dZ1l2RX1TBO-SGLgIy1ZR7q3Skxe_8jinsNYouOOdU2qQB85oCpAc4aAasXYQaXzxeo0OGypqhjDuSuxijIyfGfQBXpEVP-ehhEHQ_HpMMsb34KaaGK8A9751R-XoK4I75siWyjoU01gZlPmQZHK7LTsAZE2vEFtExnxAqf8z50VjHiEfdXqOkxKkhfQSwt3ntu0yOw-gi7R29i_jGcXGmxv3BEt2tKrj4GQEqqNIdGx27Oh8TX9bGsmrFgP6oyRDixSyvtdZrlEI2zR9E7dHLQ8KwP3JVOrhCt-5Y11VeS0FUYloe0eRTuoxblrkJ6gxkBFZhmbU0R_sLFJ2HzC-94TpfWjAWFNGo8cWDOcdHLFLgvT-YqJT-Bv0xHbS=w1000-h1400-no?authuser=0"
+  },
+  eshika_saxena: {
+      "id": "eshika_saxena",
+      "name": "Eshika Saxena",
+      "year": 2023,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/eF7YoVhmWFeP-v4CJV-bvanKvW307Gro681e7nsd0raA2pI0oBiIPNklhvH_Qgl9Z3MvlNBZWcz2CpYkQq_uMq2xnqzavRCLJSjeqg6gfZLu1C0U3LFJJlU97vnVWW9nE5z90LeyKmihDjr8KAAZDaHB2J4Br3q_ec-cgGKLRmtTlp0b6mcSbgjInaIGWpCEPtkRx09I9DsEL0qqi_Gg9nbPUSibLU-ClDp4EePp8QAFap2_b1QZtWLl-Do97ptyT0H4X22DgJqPO-QnymOzbfjKT-HpSckZf64AMF6Y-arXpI0dk5e_s5beHdEVrt_ASyOG3ePpBEJJGyIs4mV7PL_4EfjvHez4vC_FwQTKg-RZxDf7cyMCD3Yo5npdbSG2JJTCIb27C5mkN_e6a4UYHcnVTj6Z3q1IgS1QKYZr80JvhvxOSUVf060UPdscjkAcQHB1Pd_24WQKQ7m-0z45ZDuMCVXNe-V74joyuZGbXHawWP7BCJh_EQERurc2nrqRPsRbUcZp-1nTPDu0tXEqy5ZjOcnQvJBVHCISmJUgFSQoQBy2dxn31sGPLC-OEvL0CiC5nYUW022E2Tm_1Q1QRrdjYep5EfTzLuxJW4XcjP9eLSWM2BIKovsZN55sh0qLaClr8TrfsnGhFfGIJ37muOKl99BC3yZ-dTBaNTCdiCuUlHik1RHnZa2__cPJrcf9aUfBh2VcQy8BixAHLLEVmSaT=w1000-h1400-no?authuser=0"
+  },
+  isabelle_zheng: {
+      "id": "isabelle_zheng",
+      "name": "Isabelle Zheng",
+      "year": 2022,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/paIVDLHucaUxgQn2V4VAnkXXexQ2JIbdiWJSWh8m_hM5VHUKG3y27-oaYZtCDTDVuaXznvsG9xbG1_0uoTOG4AnkIWC4mgI3G-fLzTZNXT8H1euIPOxuFh3lLDZo1g3jFSTQdFWyKHIZUqrAm20cCiVz3s2rf1RYU-JG6NIXFKYusSja9bs2dXThiPaGC-aOqczYlEj-FG8gScIaCMeJUOdryCYpndcEOnLNjpvT1KLYH8em6rui5wost7GV5lre7zsg7kK3P1oJXYFu7NeblWDhKsB7fnU5eFi2lVzAMLrioVs5xjz1VLA0HCdAQeThuRJXoVgh1si00VCaCirvjgKVg8TWZTjmxAJ1aoQmLLHbTzYCbJlHLhP0B0TbqrmOy8D6g1zV8Rj8uz2Ni520zw7IAiHiQhXx92pjlDA0XHp6DaiN4eW_JvrDHY5sF_MNRJAupB9dJhYW4olC6OaoWhrrf50woXeASnl-RseQXW__fceiZKxDt_sKJL3jmcgZGYsC9gGwKk4lvXBw_bTIk5DBl77nYIdeUz0mWofdZHEiug3BhUXgtvDt1aIxHYu6_tMFC716huoVTVgMvDTsBmtVbkWy0RCAzPwb6CM5Sv3afsf5yFbVhRAvx74k1Ifq8AAVpSdWo7sRMVI7oz5-Id7gN-n62Gg-DHqvZcmAejJBgzGiu2nE7622SBXsDASbUBvGnnd_-g3t-B6uqct4Hin4=w1000-h1400-no?authuser=0"
+  },
+  lauren_chen: {
+      "id": "lauren_chen",
+      "name": "Lauren Chen",
+      "year": 2024,
+      "pronouns": "she/her/hers",
+      "quote": "asdf",
+      "photostat": "https://lh3.googleusercontent.com/h1GK4S0VQVfnRqQa5w0vD6tM05UkS87EtPDziWwdc9rd3lkw1pD5r6BdAfO_-j_24Cq3fe-LT-amh4C22bZhK_Bf80DmUAUOPaKm3fd-GPeM0IfNC6JmKhIawfQEBRoYdTwF2EP65oGyrAan2u1lUmfxwYRW43XO-qGLlNp5GG_BfplSklF0zu0UZXwYcLFH_kxKtMkbnTMpXbuGqtUUVpTWKw9Q5LccxaxXGW6lIKRR5xMGdchnY3bqWWFy29HukU9IaCL-6sjEVTnp10p2hkBYqbCm1EiJ6NhzXi_eJLDn2Ua2W9CFjJ8VOyNtup5cVHWIiB5HlS5GR-gGojjPgnlvm2BszYMUJKMYlZVGS0Dlli0-zFHUvbCHqbEV3rpNaTK8Ll7dyscqNZ-z0hPw9ldHYlBSPdi74hRT5puFJ9MCWntYzHiyCn0FH8fnZXbixamFYGUmMq88CnvSKUfjSYWIqJJl7mC8HI1h3TnkcyorQvn4-SbIQ08fqiHwNfgx0Ye3Rb0dm8NKni881o0vLPetGikxZmSg0LxKBGwrYrjk4AiOPuZrjjE4CHZJcPqfoCNWpaHQKYXv6yGtuOUzq409jm-OtJFiMdapJMIScjtm0c_0vLy0inplQQ2WqyIpQ1VsTM_lcMf3nGer5AF3wrSxlu3b1nwWa20FIFIcxoezE0RFusdL2456MImqmFbAtg7RPG6SsIqdUaECUlXtC_gW=w1000-h1400-no?authuser=0"
+  }
+}
+
+export const features;
